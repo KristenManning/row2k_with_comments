9384b79ae9604424fd00d21e5748bcb5eff78ae2
Kristen Manning <kristenm@Kristens-MacBook-Pro.local>
2017-04-04 22:13:56 -0400

    getting handlebars and mongoose working with basic print statements

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-/* Scraper Template  (18.10) 
- * ========================= */
+// Dependencies
 
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Race = require("./models/Race.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 
-// Students: Using this skeleton, the cheerio documentation,
-// and what you've learned in class so far, scrape a website
-// of your choice, save it in a result array, and log it to the console.
+// ------------------------------------------------
+// Initialize Express
 
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express.static("public"));
 
-// Dependencies:
+// ------------------------------------------------
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// ------------------------------------------------
+// Database configuration with mongoose
+
+mongoose.connect("mongodb://localhost/row2kdb");
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// ------------------------------------------------
 
 // Snatches HTML from URLs
 var request = require('request');
@@ -17,7 +50,7 @@ var cheerio = require('cheerio');
 // Make a request call to grab the HTML body from the site of your choice
 request('http://www.row2k.com/results/', function (error, response, html) {
 
-	// Load the HTML into cheerio and save it to a variable
+  // Load the HTML into cheerio and save it to a variable
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(html);
 
@@ -39,4 +72,24 @@ request('http://www.row2k.com/results/', function (error, response, html) {
     });
     });
   console.log(result);
+});
+
+// This will get the articles we scraped from the mongoDB
+app.get("/stuff", function(req, res) {
+  // Grab every doc in the Articles array
+  Race.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.render("index", {doc});
+    }
+  });
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
